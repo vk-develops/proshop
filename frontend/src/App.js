14d31450b052ba65075d6b13da407e5dbb25a7e5
vk-develops <vimalkumarv291@gmail.com>
2023-09-08 21:25:44 +0530

    Added Header, Footer, Main components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+ 
 const App = () => {
     return (
         <>
-            <h1>Hello World!!!</h1>
-            <h2>ifhdf</h2>
+        <Header />
+        <main className='py-3'>
+            <Container>
+                <h1>Welcome to proshop</h1>
+            </Container>
+        </main>
+        <Footer />
         </>
     );
 }
