2c1009bf96028033966013d5e95c608baa65b5fc
vk-develops <vimalkumarv291@gmail.com>
2023-09-08 20:45:27 +0530

    App initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello World!!!</h1>
+            <h2>ifhdf</h2>
+        </>
+    );
+}
+
+export default App;
